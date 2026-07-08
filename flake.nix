cceff779fec1dbaaf4c1fbc1b02eee23e543a793
DrunkSatyr <caro@drunksatyr.dev>
2026-07-08 14:02:25 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+    url = "github:0xc000022070/zen-browser-flake";
+    inputs = {
+      # IMPORTANT: To ensure compatibility with the latest Firefox version, use nixpkgs-unstable.
+      nixpkgs.follows = "nixpkgs";
+      home-manager.follows = "home-manager";
+    };
+  };
+
+  };
+
+  outputs = { nixpkgs, home-manager, zen-browser, ... } @ inputs:
+  {
+   nixosConfigurations.sheepytower = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+
+      modules = [
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.backupFileExtension = "HMBackup";
+              home-manager.useUserPackages = true;
+              home-manager.users.caro.imports = [
+                ./home.nix
+              ];
+              home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
+            }
+          ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, system, inputs, ... }:
+
+{
+    imports = [
+        inputs.zen-browser.homeModules.beta
+        # or inputs.zen-browser.homeModules.twilight
+        # or inputs.zen-browser.homeModules.twilight-official
+    ];
+
+    home.username = "caro";
+    home.homeDirectory = "/home/caro";
+
+    # Packages that should be installed to the user profile.
+    home.packages = with pkgs; [
+        zip
+        xz
+        unzip
+        p7zip
+        oh-my-zsh
+        oh-my-posh
+    ];
+
+    # basic configuration of git, please change to your own
+    programs.git = {
+        enable = true;
+        userName = "DrunkSatyr";
+        userEmail = "caro@drunksatyr.dev";
+        extraConfig = {
+            init.defaultBranch = "main";
+        };
+    };
+
+    programs.zen-browser = {
+        enable = true;
+        setAsDefaultBrowser = true;
+    };
+
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "26.05";
+
+    # Let home Manager install and manage itself.
+    programs.home-manager.enable = true;
+}
