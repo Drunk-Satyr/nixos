dbb737f32926133f33b3c9e4d6232fad6dc29c07
DrunkSatyr <caro@drunksatyr.dev>
2026-07-10 12:44:18 -0700

    fixed permission issue on smb mount

--- network.nix
+++ network.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   # For mount.cifs, required unless domain name resolution is not needed.
@@ -12,6 +12,12 @@
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
+
+  networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
+
+  networking.extraHosts = ''
+    10.10.0.126 sleepysheepy # Home Server
+  '';
 }
