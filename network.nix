bca54e9360251174784d610dc4683ff8e9f66f25
DrunkSatyr <caro@drunksatyr.dev>
2026-07-11 21:46:12 -0700

    worked on samba mount settings

--- network.nix
+++ network.nix
@@ -1,21 +1,26 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/media" = {
+  fileSystems."/mnt/sleepymedia" = {
     device = "//10.10.0.126/sleepymedia";
     fsType = "cifs";
     options =
       let
         # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        automount_opts = "x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
       in
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
 
-  networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
+  # networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
 
   networking.extraHosts = ''
     10.10.0.126 sleepysheepy # Home Server
