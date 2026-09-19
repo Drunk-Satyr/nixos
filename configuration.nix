6734674c2eef26f5842b96d129c60808cd045995
DrunkSatyr <caro@drunksatyr.dev>
2026-09-18 22:35:48 -0700

    prevent automated updates if there are unstaged changes

--- configuration.nix
+++ configuration.nix
@@ -171,6 +171,7 @@
   systemd.services."update-nixos" = {
     script = ''
       set -eu
+      [[ $(git status | grep "Changes not staged for commit:") ]]
       nix flake update --commit-lock-file
       nixos-rebuild switch
       nix-collect-garbage --delete-older-than 14d
